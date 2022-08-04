51b5fe69b819874887c0ea272daa5ee689a83518
Yiwei Chen <kanyeishere@gmail.com>
2022-08-04 20:54:14 +1000

    update install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,8 @@
-pip install nvidia-pyindex
-pip install -r requirements.txt
+pip3 install nvidia-pyindex
+pip3 install -r requirements.txt
 git clone https://github.com/Megvii-BaseDetection/YOLOX.git
 cd YOLOX
-pip install -U pip && pip install -r requirements.txt
-pip install -v -e .
+pip3 install -U pip && pip3 install -r requirements.txt
+pip3 install -v -e .
 cd ..
 python3 setup.py
