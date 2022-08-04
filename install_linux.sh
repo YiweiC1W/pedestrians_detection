d75adaa77f9c301efaabb1611bb2a980ad3fa0b8
Yiwei Chen <kanyeishere@gmail.com>
2022-08-05 00:11:46 +1000

    update install

--- install_linux.sh
+++ install_linux.sh
@@ -1,12 +1,10 @@
 pip3 install nvidia-pyindex
-pip3 install -r 'requirements.txt'
-
+pip3 install -r requirements.txt
 python3 -m pip install -U scikit-image
 git clone https://github.com/YiweiC1W/9517dataset.git
 git clone https://github.com/Megvii-BaseDetection/YOLOX.git
 cd YOLOX
-pip3 install -U pip && pip3 install -r 'requirements.txt'
-
+pip3 install -U pip && pip3 install -r requirements.txt
 pip3 install -v -e .
 cd ..
 python3 setup.py
--- install_win64.bat
+++ install_win64.bat
@@ -1,12 +1,10 @@
 pip install nvidia-pyindex
-pip install -r 'requirements.txt'
-
+pip install -r requirements.txt
 python -m pip install -U scikit-image
 git clone https://github.com/YiweiC1W/9517dataset.git
 git clone https://github.com/Megvii-BaseDetection/YOLOX.git
 cd YOLOX
-pip install -U pip && pip install -r 'requirements.txt'
-
+pip install -U pip && pip install -r requirements.txt
 pip install -v -e .
 cd ..
 python setup.py
